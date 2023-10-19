2a253b4644d297f32951c29d0884ad20f5a2aa5d
DwiN3 <dwinicht333@gmail.com>
2023-10-19 14:25:23 +0200

    dodanie zad5

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-const _ = require('lodash');
-const user = require('./user');
+function getMails(collections) {
+  const pattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
+  
+  const mails = collections
+    .filter((item) => typeof item === 'string')
+    .map((str) => str.match(pattern))
+    .flat()
+  
+  const filteredMails = mails.filter((mail) => mail);
+  const sortedMails = [...new Set(filteredMails)].sort();
+  return sortedMails
+}
 
-const subject_w1 = _.find(user.allGrades, { weight: 1 });
+const collections = [
+  {},
+  15,
+  "hello@test.pl",
+  null,
+  ['aaa', 'bbb', 5],
+  'admin@gmail.com',
+  undefined,
+  'a34@yahoo.com',
+  '321@a',
+  '321.pl'
+];
 
-if (subject_w1) console.log(`Przedmiot o wadze 1: ${subject_w1.subjectName}`);
-else console.log('Nie znaleziono przedmiotu o wadze 1.');
+const Mails = getMails(collections);
+console.log(Mails);
