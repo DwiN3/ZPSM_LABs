282706082742c927fc6c9d846354b7847a67f5ab
DwiN3 <dwinicht333@gmail.com>
2023-10-19 23:23:12 +0200

    dodanie zad5

--- app.js
+++
@@ -1,7 +0,0 @@
-const _ = require('lodash');
-const user = require('./user');
-
-const subject_w1 = _.find(user.allGrades, { weight: 1 });
-
-if (subject_w1) console.log(`Przedmiot o wadze 1: ${subject_w1.subjectName}`);
-else console.log('Nie znaleziono przedmiotu o wadze 1.');
