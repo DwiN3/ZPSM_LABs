1dc70489f557c8c8b06c1ab02174a5bc08f5dcfd
DwiN3 <dwinicht333@gmail.com>
2023-11-03 04:55:05 +0100

    ustawienie przycisków

--- Calculator_35196/App.tsx
+++ Calculator_35196/App.tsx
@@ -388,7 +388,7 @@ const App = () => {
     };
   }, []);
 
-  const buttonsPortrait2 = [
+  const buttonsPortrait = [
     { title: 'AC', onPress: clearDisplay, style: styles.buttonClear },
     { title: '', style: styles.buttonClear },
     { title: '', style: styles.buttonClear },
@@ -410,7 +410,7 @@ const App = () => {
     { title: '=', onPress: calculateResult, style: styles.buttonOperator },
   ];
 
-  const buttonsPortrait = [
+  const buttonsLand = [
     { title: '(', onPress: () => handleButtonPress('('), style: styles.buttonClear },
     { title: ')', onPress: () => handleButtonPress(')'), style: styles.buttonClear },
     { title: 'mc', onPress: () => clearMemory(), style: styles.buttonClear },
@@ -420,45 +420,56 @@ const App = () => {
     { title: 'AC', onPress: () => clearDisplay(), style: styles.buttonClear },
     { title: '+/-', onPress: () => minus_plus(), style: styles.buttonClear },
     { title: '%', onPress: () => procent(), style: styles.buttonClear },
-    { title: '/', onPress: () => handleButtonPress('/'), style: styles.buttonOperator },
-    { title: '2nd', onPress: () => power2nd(), style: styles.buttonOperator },
-    { title: 'e^x', onPress: () => expPower(), style: styles.buttonOperator },
-    { title: '10^x', onPress: () => powerOfTen(), style: styles.buttonOperator },
+    { title: '/', onPress: () => handleButtonPress('/'), style: styles.buttonClear },
+
+    { title: '', onPress: () => power2nd(), style: styles.buttonClear },
+    { title: '', onPress: () => power2nd(), style: styles.buttonClear },
+    { title: '', onPress: () => power2nd(), style: styles.buttonClear },
+    { title: '2nd', onPress: () => power2nd(), style: styles.buttonClear },
+    { title: 'e^x', onPress: () => expPower(), style: styles.buttonClear },
+    { title: '10^x', onPress: () => powerOfTen(), style: styles.buttonClear },
     { title: '7', onPress: () => handleButtonPress('7'), style: styles.buttonNumber },
     { title: '8', onPress: () => handleButtonPress('8'), style: styles.buttonNumber },
     { title: '9', onPress: () => handleButtonPress('9'), style: styles.buttonNumber },
     { title: '*', onPress: () => handleButtonPress('*'), style: styles.buttonOperator },
-    { title: '1/x', onPress: () => reciprocal(), style: styles.buttonOperator },
-    { title: '√x', onPress: () => sqrt('√'), style: styles.buttonOperator },
-    { title: '³√x', onPress: () => sqrt('³√'), style: styles.buttonOperator },
-    { title: 'y√x', onPress: () => sqrt('y√x'), style: styles.buttonOperator },
-    { title: 'In', onPress: () => ln(), style: styles.buttonOperator },
-    { title: 'log10', onPress: () => log10(), style: styles.buttonOperator },
+
+    { title: '1/x', onPress: () => reciprocal(), style: styles.buttonClear },
+    { title: '√x', onPress: () => sqrt('√'), style: styles.buttonClear },
+    { title: '³√x', onPress: () => sqrt('³√'), style: styles.buttonClear },
+    { title: 'y√x', onPress: () => sqrt('y√x'), style: styles.buttonClear },
+    { title: 'In', onPress: () => ln(), style: styles.buttonClear },
+    { title: 'log10', onPress: () => log10(), style: styles.buttonClear },
     { title: '4', onPress: () => handleButtonPress('4'), style: styles.buttonNumber },
     { title: '5', onPress: () => handleButtonPress('5'), style: styles.buttonNumber },
     { title: '6', onPress: () => handleButtonPress('6'), style: styles.buttonNumber },
     { title: '-', onPress: () => handleButtonPress('-'), style: styles.buttonOperator },
-    { title: 'x!', onPress: () => factorial(), style: styles.buttonOperator },
-    { title: 'sin', onPress: () => trigFunction('sin'), style: styles.buttonOperator },
-    { title: 'cos', onPress: () => trigFunction('cos'), style: styles.buttonOperator },
-    { title: 'tan', onPress: () => trigFunction('tan'), style: styles.buttonOperator },
-    { title: 'e', onPress: () => displayEuler(), style: styles.buttonOperator },
-    { title: 'EE', onPress: () => scientificNotation(), style: styles.buttonOperator },
+
+    { title: 'x!', onPress: () => factorial(), style: styles.buttonClear },
+    { title: 'sin', onPress: () => trigFunction('sin'), style: styles.buttonClear },
+    { title: 'cos', onPress: () => trigFunction('cos'), style: styles.buttonClear },
+    { title: 'tan', onPress: () => trigFunction('tan'), style: styles.buttonClear },
+    { title: 'e', onPress: () => displayEuler(), style: styles.buttonClear },
+    { title: 'EE', onPress: () => scientificNotation(), style: styles.buttonClear },
     { title: '1', onPress: () => handleButtonPress('1'), style: styles.buttonNumber },
     { title: '2', onPress: () => handleButtonPress('2'), style: styles.buttonNumber },
     { title: '3', onPress: () => handleButtonPress('3'), style: styles.buttonNumber },
     { title: '+', onPress: () => handleButtonPress('+'), style: styles.buttonOperator },
-    { title: 'Rad', onPress: () => degreesToRadians(), style: styles.buttonOperator },
-    { title: 'sinh', onPress: () => trigFunction('sinh'), style: styles.buttonOperator },
-    { title: 'cosh', onPress: () => trigFunction('cosh'), style: styles.buttonOperator },
-    { title: 'tanh', onPress: () => trigFunction('tanh'), style: styles.buttonOperator },
-    { title: 'π', onPress: () => displayPi(), style: styles.buttonOperator },
-    { title: 'Rand', onPress: () => random(), style: styles.buttonOperator },
+
+    { title: 'Rad', onPress: () => degreesToRadians(), style: styles.buttonClear },
+    { title: 'sinh', onPress: () => trigFunction('sinh'), style: styles.buttonClear },
+    { title: 'cosh', onPress: () => trigFunction('cosh'), style: styles.buttonClear },
+    { title: 'tanh', onPress: () => trigFunction('tanh'), style: styles.buttonClear },
+    { title: 'π', onPress: () => displayPi(), style: styles.buttonClear },
+    { title: 'Rand', onPress: () => random(), style:[ styles.buttonClear, { flex:1 } ]},
     { title: '0', onPress: () => handleButtonPress('0'), style: [styles.buttonZero, { flex: 2 }],},
     { title: '.', onPress: () => handleButtonPress('.'), style: styles.buttonNumber },
     { title: '=', onPress: () => calculateResult(), style: styles.buttonOperator },
   ];
-  
+  const buttonsToRender = orientation === 'landscape' ? buttonsLand : buttonsPortrait;
+  const sliceCount = orientation === 'landscape' ? 10 : 4;
+  const sizeFont = orientation === 'landscape'
+  ? { ...styles.buttonText, fontSize: 15 }
+  : { ...styles.buttonText, fontSize: 35 };
 
   return (
     <View style={styles.container}>
@@ -468,13 +479,13 @@ const App = () => {
       <View style={styles.buttons}>
         {[0, 1, 2, 3, 4].map((row) => (
           <View key={row} style={styles.row}>
-            {buttonsPortrait.slice(row * 4, (row + 1) * 4).map((buttonsPortrait, index) => (
+            {buttonsToRender.slice(row * sliceCount, (row + 1) * sliceCount).map((button, index) => (
               <TouchableOpacity
                 key={index}
-                style={buttonsPortrait.style}
-                onPress={buttonsPortrait.onPress}
+                style={button.style}
+                onPress={button.onPress}
               >
-                <Text style={styles.buttonText}>{buttonsPortrait.title}</Text>
+                <Text style={sizeFont}>{button.title}</Text>
               </TouchableOpacity>
             ))}
           </View>
@@ -510,7 +521,7 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: 'white',
-    fontSize: 35,
+    fontSize: 10,
     paddingVertical: 20,
     textAlign: 'center',
   },
