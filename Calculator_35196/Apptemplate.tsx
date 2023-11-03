875a0aa69e60294c57237c80e12720884f60a60d
DwiN3 <dwinicht333@gmail.com>
2023-11-03 02:56:15 +0100

    stworzenie lab5

---
+++ Calculator_35196/Apptemplate.tsx
@@ -0,0 +1,36 @@
+const buttons = [
+    {
+        backgroundColor: '#636466'
+        title: 'AC',
+        disable: false,
+        borderColor: '#555759'
+        onPress: () => {
+
+        },
+        color: '#E8E9EA'
+    },
+    {
+        backgroundColor: '#636466'
+        title: '',
+        disable: true,
+        borderColor: 'transparent',
+        onPress: () => {},
+        color: 'transparent'
+    },
+    {
+        backgroundColor: '#636466'
+        title: ',',
+        disable: true,
+        borderColor: '#555759',
+        onPress: () => {},
+        color: '#E8E9EA'
+    },
+    {
+        backgroundColor: '#636466'
+        title: '=',
+        disable: false,
+        borderColor: '#555759'
+        onPress: () => { },
+        color: '#E8E9EA'
+    }
+]
