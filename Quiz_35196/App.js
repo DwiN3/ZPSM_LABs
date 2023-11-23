73f3e91003d1f99891c62d1d83789d541347c105
DwiN3 <dwinicht333@gmail.com>
2023-11-24 00:24:43 +0100

    wyświetlanie testów

--- Quiz_35196/App.js
+++ Quiz_35196/App.js
@@ -1,15 +1,48 @@
 import React, { useRef } from 'react';
-import { Button, Text, View, StyleSheet, Image } from 'react-native';
+import { Button, Text, View, StyleSheet, Image, FlatList, TouchableOpacity } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { TestsList } from './data/Tests';
 
 const Drawer = createDrawerNavigator();
 
-const HomeScreen = ({ navigation }) => (
-  <View style={styles.container}>
-    <Text style={styles.centeredText}>Home Page</Text>
-  </View>
-);
+const HomePage = ({ navigation }) => {
+  const truncateText = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return text.substring(0, maxLength - 3) + '...';
+    } else {
+      return text;
+    }
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => navigation.navigate('TestDetails', { test: item })}>
+      <View style={styles.testItem}>
+        <Text style={styles.titleTest}>{item.titleTest}</Text>
+        <View style={styles.tagsContainer}>
+          {item.tags.map((tag, index) => (
+            <TouchableOpacity key={index} onPress={() => console.log(`Pressed ${tag}`)}>
+              <Text style={styles.tag}>{tag}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <Text style={styles.description}>{truncateText(item.description, 100)}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={TestsList}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.titleTest}
+        contentContainerStyle={styles.flatListContainer}
+      />
+    </View>
+  );
+};
+
 
 const Results = ({ navigation }) => (
   <View style={styles.container}>
@@ -27,18 +60,35 @@ const DrawerContent = ({ navigation }) => (
     <View style={styles.buttonContainer}>
       <Button
         title="Home Page"
-        onPress={() => navigation.navigate('Home Screen')}
-        color="#808080" // Szare tło przycisku
+        onPress={() => navigation.navigate('Home Page')}
+        color="#808080"
       />
       <View style={styles.buttonSpacer} />
       <Button
         title="Results"
         onPress={() => navigation.navigate('Results')}
-        color="#808080" // Szare tło przycisku
+        color="#808080" 
       />
     </View>
   </View>
 );
+
+const TestDetails = ({ route }) => {
+  const { test } = route.params;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.centeredText}>{test.titleTest}</Text>
+      <View style={styles.tagsContainer}>
+        {test.tags.map((tag, index) => (
+          <TouchableOpacity key={index} onPress={() => console.log(`Pressed ${tag}`)}>
+            <Text style={styles.tag}>{tag}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+      <Text style={styles.description}>{test.description}</Text>
+    </View>
+  );
+};
 
 const App = () => {
   const drawer = useRef(null);
@@ -46,8 +96,9 @@ const App = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator drawerContent={(props) => <DrawerContent {...props} />}>
-        <Drawer.Screen name="Home Screen" component={HomeScreen} />
+        <Drawer.Screen name="Home Page" component={HomePage} />
         <Drawer.Screen name="Results" component={Results} />
+        <Drawer.Screen name="TestDetails" component={TestDetails} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
@@ -59,6 +110,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     padding: 16,
+  },
+  flatListContainer: {
+    flexGrow: 1,
   },
   navigationContainer: {
     backgroundColor: '#ecf0f1',
@@ -85,6 +139,33 @@ const styles = StyleSheet.create({
   },
   buttonSpacer: {
     marginVertical: 10, 
+  },
+  testItem: {
+    marginVertical: 10,
+    padding: 15,
+    width: '100%', // Zmiana szerokości na 100%
+    height: 150, // Ustawienie stałej wysokości
+    borderWidth: 3,
+    borderColor: '#ccc',
+    borderRadius: 5,
+  },
+  titleTest: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  tagsContainer: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  tag: {
+    fontSize: 14,
+    marginRight: 5,
+    color: 'blue',
+  },
+  description: {
+    marginTop: 11,
+    fontSize: 14,
   },
 });
 
