6d2eb3909a24cd66aa6e154e624ce87370ffc7b9
DwiN3 <dwinicht333@gmail.com>
2023-11-24 17:27:27 +0100

    dodanie tabeli w results

--- Quiz_35196/data/UserScores.js
+++ Quiz_35196/data/UserScores.js
@@ -10,10 +10,16 @@ class UserScores {
   }
 
   const userScores = [
-    new UserScores("asdf", "18/20", "test1", "21-11-2018"),
-    new UserScores("asdf1", "15/20", "test1", "21-11-2018"),
-    new UserScores("asdf3", "12/20", "test1", "21-11-2018"),
-    new UserScores("asdf4", "20/20", "test1", "21-11-2018"),
+    new UserScores("God", "20/20", "test1", "21-11-1990"),
+    new UserScores("Master", "18/20", "test1", "18-11-1995"),
+    new UserScores("Soul", "16/20", "test4", "11-11-1985"),
+    new UserScores("Half", "13/20", "test2", "15-11-2000"),
+    new UserScores("Qwerty", "9/20", "test5", "15-11-1998"),
+    new UserScores("xcz", "8/20", "test4", "15-11-1982"),
+    new UserScores("xyz", "5/20", "test1", "15-11-1976"),
+    new UserScores("nobek", "3/20", "test5", "15-11-1993"),
+    new UserScores("Bad man", "1/20", "test2", "15-11-1988"),
+    new UserScores("Giga Noob", "0/20", "test1", "15-11-1999"),
   ];
 
   export { UserScores, userScores };
--- Quiz_35196/styles/ResultsStyle.js
+++ Quiz_35196/styles/ResultsStyle.js
@@ -3,12 +3,27 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  containerResults: {
-    flex: 1,
-    alignItems: 'center',
+  scrollContainer: {
+    
   },
-  centeredText: {
-    fontSize: 18,
+  row: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+    paddingVertical: 8,
+  },
+  header: {
+    backgroundColor: '#808080',
+  },
+  evenRow: {
+    backgroundColor: '#fff', // biały kolor tła
+  },
+  oddRow: {
+    backgroundColor: '#f2f2f2', // jasno szary kolor tła
+  },
+  cell: {
+    flex: 1,
+    color: 'black',
     textAlign: 'center',
   },
 });
