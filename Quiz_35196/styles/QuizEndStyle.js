21517fbde53c882f1eb138278335b7780f6236f5
DwiN3 <dwinicht333@gmail.com>
2023-12-01 19:23:34 +0100

    dodanie css

---
+++ Quiz_35196/styles/QuizEndStyle.js
@@ -0,0 +1,28 @@
+// QuizEndStyle.js
+
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#fff', // Adjust background color as needed
+  },
+  title: {
+    marginTop: 20,
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  titleTest:
+  {
+    fontSize: 20,
+  },
+  text: {
+    marginTop: 5,
+    fontSize: 18,
+    marginBottom: 10,
+  },
+});
+
+export default styles;
