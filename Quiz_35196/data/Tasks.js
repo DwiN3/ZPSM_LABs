c29b1d257204bf0190cd374e5048c1298f406953
DwiN3 <dwinicht333@gmail.com>
2023-12-01 14:19:33 +0100

    przygotowania do quiz

---
+++ Quiz_35196/data/Tasks.js
@@ -0,0 +1,93 @@
+const tasks = [
+    {
+      "question": "Który wódz po śmierci Gajusza Mariusza, prowadził wojnę domową z Sullą?",
+      "answers": [
+        {
+          "content": "LUCJUSZ CYNNA",
+          "isCorrect": true
+        },
+        {
+          "content": "JULIUSZ CEZAR",
+          "isCorrect": false
+        },
+        {
+          "content": "LUCJUSZ MURENA",
+          "isCorrect": false
+        },
+        {
+          "content": "MAREK KRASSUS",
+          "isCorrect": false
+        }
+      ],
+      "duration": 30
+    },
+    {
+      "question": "Kto był pierwszym królem Polski?",
+      "answers": [
+        {
+          "content": "BOLESLAW CHROBRY",
+          "isCorrect": false
+        },
+        {
+          "content": "MIESZKO I",
+          "isCorrect": true
+        },
+        {
+          "content": "KAZIMIERZ WIELKI",
+          "isCorrect": false
+        },
+        {
+          "content": "JAN III SOBIESKI",
+          "isCorrect": false
+        }
+      ],
+      "duration": 45
+    },
+    {
+      "question": "Które miasto jest stolicą Japonii?",
+      "answers": [
+        {
+          "content": "SEOUL",
+          "isCorrect": false
+        },
+        {
+          "content": "BEIJING",
+          "isCorrect": false
+        },
+        {
+          "content": "TOKIO",
+          "isCorrect": true
+        },
+        {
+          "content": "HANOI",
+          "isCorrect": false
+        }
+      ],
+      "duration": 20
+    },
+    {
+      "question": "Który pierwiastek chemiczny ma symbol 'O'?",
+      "answers": [
+        {
+          "content": "TLLEN",
+          "isCorrect": false
+        },
+        {
+          "content": "OXYGEN",
+          "isCorrect": true
+        },
+        {
+          "content": "GOLD",
+          "isCorrect": false
+        },
+        {
+          "content": "IRON",
+          "isCorrect": false
+        }
+      ],
+      "duration": 25
+    }
+  ];
+  
+  export { tasks };
+  
---
+++ Quiz_35196/screens/ResultsQuizScreen.js
@@ -0,0 +1,16 @@
+// ResultsQuizScreen.js
+
+import React from 'react';
+import { ScrollView, View, Text } from 'react-native';
+import styles from '../styles/ResultsQuizStyle';
+import { userScores } from '../data/UserScores';
+
+const ResultsQuiz = () => (
+
+  <View style={styles.container}>
+    <Text>Koniec Quizu {TitleTest}</Text>
+    <Text>Poprawne odpowiedzi: </Text>
+  </View>
+);
+
+export default ResultsQuiz;
