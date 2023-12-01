a5ae45f1ddf5a4f7de7c7916b0426a6380132aed
DwiN3 <dwinicht333@gmail.com>
2023-12-01 13:57:35 +0100

    działa results

--- Quiz_35196/data/UserScores.js
+++ Quiz_35196/data/UserScores.js
@@ -1,25 +1,76 @@
 // UserScore.js
 
-class UserScores {
-    constructor(nick, point, type, date) {
-      this.nick = nick;
-      this.point = point;
-      this.type = type;
-      this.date = date;
-    }
-  }
+const userScores = [
+  {
+    "nick": "God",
+    "score": 20,
+    "total": 20,
+    "type": "test1",
+    "date": "21-11-1990"
+  },
+  {
+    "nick": "Master",
+    "score": 18,
+    "total": 20,
+    "type": "test1",
+    "date": "18-11-1995"
+  },
+  {
+    "nick": "Soul",
+    "score": 16,
+    "total": 20,
+    "type": "test4",
+    "date": "11-11-1985"
+  },
+  {
+    "nick": "Half",
+    "score": 13,
+    "total": 20,
+    "type": "test2",
+    "date": "15-11-2000"
+  },
+  {
+    "nick": "Qwerty",
+    "score": 9,
+    "total": 20,
+    "type": "test5",
+    "date": "15-11-1998"
+  },
+  {
+    "nick": "xcz",
+    "score": 8,
+    "total": 20,
+    "type": "test4",
+    "date": "15-11-1982"
+  },
+  {
+    "nick": "xyz",
+    "score": 5,
+    "total": 20,
+    "type": "test1",
+    "date": "15-11-1976"
+  },
+  {
+    "nick": "nobek",
+    "score": 3,
+    "total": 20,
+    "type": "test5",
+    "date": "15-11-1993"
+  },
+  {
+    "nick": "Bad man",
+    "score": 1,
+    "total": 20,
+    "type": "test2",
+    "date": "15-11-1988"
+  },
+  {
+    "nick": "Giga Noob",
+    "score": 0,
+    "total": 20,
+    "type": "test1",
+    "date": "15-11-1999"
+  },
+];
 
-  const userScores = [
-    new UserScores("God", "20/20", "test1", "21-11-1990"),
-    new UserScores("Master", "18/20", "test1", "18-11-1995"),
-    new UserScores("Soul", "16/20", "test4", "11-11-1985"),
-    new UserScores("Half", "13/20", "test2", "15-11-2000"),
-    new UserScores("Qwerty", "9/20", "test5", "15-11-1998"),
-    new UserScores("xcz", "8/20", "test4", "15-11-1982"),
-    new UserScores("xyz", "5/20", "test1", "15-11-1976"),
-    new UserScores("nobek", "3/20", "test5", "15-11-1993"),
-    new UserScores("Bad man", "1/20", "test2", "15-11-1988"),
-    new UserScores("Giga Noob", "0/20", "test1", "15-11-1999"),
-  ];
-
-  export { UserScores, userScores };
+export { userScores };
