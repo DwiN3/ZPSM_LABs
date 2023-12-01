3b4657f2540e04e5d2db79a2788513af1b07e62f
DwiN3 <dwinicht333@gmail.com>
2023-12-01 12:11:13 +0100

    dodanie regulaminu

---
+++ Quiz_35196/screens/WelcomeScreen.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/WelcomeStyle.js';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+const WelcomeStyle = () => (
+    <View style={styles.container}>
+        <View style={styles.contentContainer}>
+        <Text style={styles.label}>Aby korzystać z aplikacji musisz zaakceptować regulamin</Text>
+        <TouchableOpacity onPress={handleAcceptanceToggle}>
+            <View style={styles.checkboxContainer}>
+            <View style={[styles.checkbox, isRegulationAccepted && styles.checkedBox]} />
+            <Text>Akceptuje regulamin</Text>
+            </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleContinuePress} disabled={!isRegulationAccepted}>
+            <View style={[styles.continueButton, !isRegulationAccepted && styles.disabledButton]}>
+            <Text>Dalej</Text>
+            </View>
+        </TouchableOpacity>
+        </View>
+    </View>
+  );
+  
+  export default WelcomeStyle;
