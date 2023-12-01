3901229c3199690e61261d7f167b446897a6c6e8
DwiN3 <dwinicht333@gmail.com>
2023-12-01 15:58:58 +0100

    nowa koncepcja

--- Quiz_35196/screens/QuizScreen.js
+++ Quiz_35196/screens/QuizScreen.js
@@ -1,16 +1,16 @@
 // QuizScreen.js
 
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Alert } from 'react-native'; // Import Alert for displaying feedback
 import { useFocusEffect, useRoute } from '@react-navigation/native';
 import styles from '../styles/QuizStyle';
 
 const QuizScreen = ({ navigation }) => {
   const [progress, setProgress] = useState(0);
   const [timeElapsed, setTimeElapsed] = useState(0);
-  const [questionTime, setQuestionTime] = useState(0); // Update initial value to 0
+  const [questionTime, setQuestionTime] = useState(0);
   const [shouldStartTimer, setShouldStartTimer] = useState(true);
-  const [currentQuestion, setCurrentQuestion] = useState(0); // Track the current question
+  const [currentQuestion, setCurrentQuestion] = useState(0);
   const intervalRef = useRef(null);
 
   const route = useRoute();
@@ -21,7 +21,7 @@ const QuizScreen = ({ navigation }) => {
   useEffect(() => {
     if (titleTest) {
       navigation.setOptions({ title: titleTest });
-      setQuestionTime(tasks[currentQuestion]?.duration || 0); // Set initial question time
+      setQuestionTime(tasks[currentQuestion]?.duration || 0);
       resetTimerFunction();
     }
   }, [titleTest, currentQuestion, tasks]);
@@ -59,7 +59,7 @@ const QuizScreen = ({ navigation }) => {
         if (newTime === questionTime) {
           clearInterval(intervalRef.current);
           console.log('Koniec czasu');
-          moveToNextQuestion();
+          handleAnswer(null); // Handle the case when the time runs out
         }
 
         return newTime;
@@ -74,10 +74,35 @@ const QuizScreen = ({ navigation }) => {
     startTimer();
   };
 
+  const handleAnswer = (selectedAnswer) => {
+    // Handle the selected answer
+    clearInterval(intervalRef.current); // Stop the timer
+
+    const isCorrect = tasks[currentQuestion]?.answers[selectedAnswer]?.isCorrect || false;
+    const feedbackMessage = isCorrect ? 'Correct!' : 'Incorrect.';
+
+    Alert.alert(
+      'Answer Feedback',
+      feedbackMessage,
+      [
+        {
+          text: 'Next Question',
+          onPress: () => moveToNextQuestion(),
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
   const moveToNextQuestion = () => {
-    setCurrentQuestion((prevQuestion) => prevQuestion + 1);
-    setQuestionTime(tasks[currentQuestion + 1]?.duration || 0);
-    resetTimerFunction();
+    if (currentQuestion + 1 < tasks.length) {
+      setCurrentQuestion((prevQuestion) => prevQuestion + 1);
+      setQuestionTime(tasks[currentQuestion + 1]?.duration || 0);
+      resetTimerFunction();
+    } else {
+      // End of the quiz, you can navigate to a summary screen or take other actions
+      console.log('End of the quiz');
+    }
   };
 
   return (
@@ -94,10 +119,8 @@ const QuizScreen = ({ navigation }) => {
       </View>
       <View style={styles.answersContainer}>
         {tasks[currentQuestion]?.answers.map((answer, index) => (
-          <TouchableOpacity key={index} style={styles.answersButton}>
-            <Text style={styles.answersButtonText} onPress={() => resetTimerFunction()}>
-              {`Answer ${String.fromCharCode(65 + index)}`}
-            </Text>
+          <TouchableOpacity key={index} style={styles.answersButton} onPress={() => handleAnswer(index)}>
+            <Text style={styles.answersButtonText}>{`Answer ${String.fromCharCode(65 + index)}`}</Text>
           </TouchableOpacity>
         ))}
       </View>
