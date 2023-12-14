ff13cfe80385a564ae2e7e048232dccbb1ef5da0
DwiN3 <dwinicht333@gmail.com>
2023-12-14 23:07:50 +0100

    fhddfh

--- Quiz_35196/screens/HomePageScreen.js
+++ Quiz_35196/screens/HomePageScreen.js
@@ -1,6 +1,13 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import { View, Text, TouchableOpacity, FlatList } from 'react-native';
 import { TestsList } from '../data/Tests';
+=======
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, Text, TouchableOpacity, FlatList, RefreshControl, NetInfo } from 'react-native';
+import { Network } from 'expo';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+>>>>>>> Stashed changes
 import styles from '../styles/HomePageStyle';
 
 const truncateText = (text, maxLength) => {
@@ -12,7 +19,62 @@ const truncateText = (text, maxLength) => {
 };
 
 const HomePageScreen = ({ navigation }) => {
+<<<<<<< Updated upstream
   const TestsListWithNewItem = [...TestsList, { resultsItem: true }];
+=======
+  const [testsList, setTestsList] = useState([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const fetchTests = async () => {
+    try {
+      const response = await fetch('https://tgryl.pl/quiz/tests');
+      const data = await response.json();
+      const shuffledTests = [...data].sort(() => Math.random() - 0.5);
+      const testsWithResults = [...shuffledTests, { resultsItem: true }];
+      setTestsList(testsWithResults);
+      await AsyncStorage.setItem('testsList', JSON.stringify(testsWithResults));
+    } catch (error) {
+      console.error('Error fetching tests:', error);
+    } finally {
+      setIsRefreshing(false);
+    }
+  };
+
+  const loadTestsFromStorage = async () => {
+    try {
+      const storedTests = await AsyncStorage.getItem('testsList');
+      if (storedTests) {
+        setTestsList(JSON.parse(storedTests));
+      }
+    } catch (error) {
+      console.error('Error loading tests from AsyncStorage:', error);
+    }
+  };
+
+  const onRefresh = useCallback(async () => {
+    setIsRefreshing(true);
+    const isConnected = await checkInternetConnectivity();
+    if (isConnected) {
+      fetchTests();
+    } else {
+      loadTestsFromStorage();
+    }
+  }, []);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const isConnected = await checkInternetConnectivity();
+      if (isConnected) {
+        fetchTests();
+      } else {
+        // Use locally stored tests if no internet connection
+        loadTestsFromStorage();
+      }
+    };
+
+    loadData();
+  }, []);
+>>>>>>> Stashed changes
 
   const renderResultsItem = ({ item }) => {
     if (item.resultsItem) {
