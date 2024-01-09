96e1766dc4e9a8242ff10a5b980915c4ea38d763
DwiN3 <dwinicht333@gmail.com>
2024-01-09 23:34:48 +0100

    dodanie paska nawigacyjnego

---
+++ IoT_35196/screens/NewDeviceScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const NewDeviceScreen = () => {
+  return (
+    <View><Text>Hello, world!</Text></View>
+  );
+};
+export default NewDeviceScreen;
