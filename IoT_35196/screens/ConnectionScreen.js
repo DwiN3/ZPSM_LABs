42d6eb3c9927758a7cbb60a71b59e6039dccf543
DwiN3 <dwinicht333@gmail.com>
2024-01-16 23:18:21 +0100

    dodanie templatki

--- IoT_35196/screens/ConnectionScreen.js
+++ IoT_35196/screens/ConnectionScreen.js
@@ -1,17 +1,17 @@
-// ConnectionScreen.js
-
 import React, { Component } from 'react';
-import { Text, View, Button } from 'react-native';
+import { View, Text, Button , TouchableOpacity } from 'react-native';
 import { BleManager } from 'react-native-ble-plx';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import styles from '../styles/ConnectionStyle';
 
-class ConnectionScreen extends Component {
+class ConnectScreen extends Component {
   constructor(props) {
     super(props);
     this.manager = new BleManager();
-    this.state = {
-      device: null,
-    };
+  }
+
+  componentDidMount() {
+    this.checkBluetoothState();
   }
 
   checkBluetoothState() {
@@ -26,43 +26,93 @@ class ConnectionScreen extends Component {
   scanAndConnect() {
     this.manager.startDeviceScan(null, null, (error, device) => {
       if (error) {
-        console.log('Error', error);
+        console.log('Error during scanning:', error);
         return;
       }
-      console.log('DEVICE ', device);
+
+      console.log('Found device:', device);
       if (device.name === 'MLT-BT05') {
         this.manager.stopDeviceScan();
-        this.connectToDevice(device);
+        device.connect()
+          .then((connectedDevice) => {
+            return connectedDevice.discoverAllServicesAndCharacteristics();
+          })
+          .then((characteristic) => {
+            console.log('Device connected and characteristics discovered:', characteristic);
+
+            const deviceInfo = {
+              id: device.id,
+              serviceUUID: 'FFE0', 
+              characteristicUUID: 'FFE1', 
+            };
+
+            AsyncStorage.setItem('device', JSON.stringify(deviceInfo)).then(() => {
+              this.props.navigation.navigate('Devices');
+            });
+          })
+          .catch((error) => {
+            console.log('Error connecting to device:', error);
+          });
       }
     });
   }
 
-  connectToDevice(device) {
-    device.connect()
-      .then((connectedDevice) => {
-        return connectedDevice.discoverAllServicesAndCharacteristics();
-      })
-      .then((characteristic) => {
-        console.log('Connected to MLT-BT05', characteristic);
-        this.setState({ device: characteristic });
-      })
-      .catch((error) => {
-        console.log('Error', error);
-      });
+  changeDevice(command) {
+    AsyncStorage.getItem('device').then(device => {
+      device = JSON.parse(device);
+      if (device) {
+        this.manager.writeCharacteristicWithResponseForDevice(
+          device.id, device.serviceUUID, device.characteristicUUID, btoa(command)
+        ).then(response => {
+          console.log('response', response);
+        }).catch((error) => {
+          console.log('Error', error);
+        });
+      }
+    });
   }
 
   render() {
     return (
       <View>
-        <Button
-          title="Scan and Connect"
-          onPress={() => this.checkBluetoothState()}
-          style={styles.button}
-        />
-        <Text>{this.state.device ? `Connected to ${this.state.device.name}` : 'Not connected'}</Text>
+        <Text>Connect Screen</Text>
+        <TouchableOpacity
+          style={styles.buttonScan}
+          onPress={() => this.scanAndConnect()}
+        >
+          <Text style={styles.buttonText}>Scan and Connect</Text>
+        </TouchableOpacity>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={[styles.button, styles.redButton]}
+            onPress={() => this.changeDevice('red')}
+          >
+            <Text style={styles.buttonText}>Send Red Command</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.button, styles.greenButton]}
+            onPress={() => this.changeDevice('green')}
+          >
+            <Text style={styles.buttonText}>Send Green Command</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.button, styles.blueButton]}
+            onPress={() => this.changeDevice('blue')}
+          >
+            <Text style={styles.buttonText}>Send Blue Command</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.button, styles.offButton]}
+            onPress={() => this.changeDevice('off')}
+          >
+            <Text style={styles.buttonText}>Turn Off Command</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
 
-export default ConnectionScreen;
+
+export default ConnectScreen;
--- IoT_35196/styles/ConnectionStyle.js
+++ IoT_35196/styles/ConnectionStyle.js
@@ -1,23 +1,42 @@
 // ConnectionStyle.js
-
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    button: {
-        borderColor: 'black',
-        borderWidth: 2,
-        backgroundColor: 'red',
-        width: '100%',
-        height: 40,
-        fontWeight: '600',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-      buttonText: {
-        color: 'white',
-        fontSize: 16,
-        fontWeight: 'bold',
-      },
+  buttonContainer: {
+    marginTop: 10,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  button: {
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  buttonScan: {
+    marginTop: -20,
+    padding: 20,
+    alignItems: 'center',
+    borderRadius: 5,
+    marginBottom: 50,
+    marginHorizontal: 0,
+    backgroundColor: 'gray',
+  },
+  redButton: {
+    backgroundColor: 'red',
+  },
+  greenButton: {
+    backgroundColor: 'green',
+  },
+  blueButton: {
+    backgroundColor: 'blue',
+  },
+  offButton: {
+    backgroundColor: 'gray',
+  },
+  buttonText: {
+    color: 'white',
+  },
 });
 
 export default styles;
